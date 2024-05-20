1fa0bba6714e70cfedab3e9e828f59ffdb7c1375
kibuchijw <kibuchijw@gmail.com>
2024-05-20 13:25:04 +0300

    Add a get_hyper method that returns a dictionary containing various key-value pairs

---
+++ 0x00-pagination/tests/2-hypermedia_pagination.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+"""
+A fuunction that returns a tuple of size two
+containing a start index and an end index
+"""
+
+import csv
+import math
+from typing import List, Dict, Any
+
+
+def index_range(page, page_size):
+    """
+    Calculate the start and end indices for pagination.
+
+    Args:
+    - page: The current page number (integer).
+    - page_size: The number of items per page (integer).
+
+    Returns:
+    - A tuple (start_index, end_index) where:
+      - start_index: The index of the first item on the page.
+      - end_index: The index one past the last item on the page.
+    """
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+    return (start_index, end_index)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Return a page of the dataset."""
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page > 0
+
+        start_index, end_index = index_range(page, page_size)
+        dataset = self.dataset()
+
+        if start_index >= len(dataset):
+            return []
+
+        return dataset[start_index:end_index]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict[str, Any]:
+        """Return hypermedia information for the dataset page."""
+        data = self.get_page(page, page_size)
+        total_items = len(self.dataset())
+        total_pages = math.ceil(total_items / page_size)
+
+        next_page = page + 1 if page < total_pages else None
+        prev_page = page - 1 if page > 1 else None
+
+        return {
+            "page_size": len(data),
+            "page": page,
+            "data": data,
+            "next_page": next_page,
+            "prev_page": prev_page,
+            "total_pages": total_pages,
+        }
