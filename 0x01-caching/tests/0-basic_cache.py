029bdd217b9d30f1b09e6c45ac4e8f8e405ae909
kibuchijw <kibuchijw@gmail.com>
2024-05-22 21:27:59 +0300

    Add a class BasicCache that inherits from BaseCaching and is a caching system

---
+++ 0x01-caching/tests/0-basic_cache.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+A class BasicCache that inherits from BaseCaching and is a caching system
+"""
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """ BasicCache inherits from BaseCaching and is a basic caching system without limit """
+
+    def put(self, key, item):
+        """ Add an item in the cache """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get an item by key """
+        if key is not None:
+            return self.cache_data.get(key)
+        return None
+
+
+# Example usage
+if __name__ == "__main__":
+    basic_cache = BasicCache()
