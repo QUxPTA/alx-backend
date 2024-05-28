7a7d0df344b137967bc95fce3a49260f55332a86
kibuchijw <kibuchijw@gmail.com>
2024-05-28 18:42:42 +0300

    Add a basic Flask app in 0-app.py and create a single / route and an index.html template

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Basic Flask app
+"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    """
+    Render the index.html template for the root route.
+
+    Returns:
+        str: Rendered HTML of the index.html template.
+    """
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
